8f18fe6031ba08478f312d583814b19b7dfe1ed8
pritambaldota <pritam@pritambaldota.com>
2015-05-31 15:08:55 +0530

    Added Nuget Package and documentation screens

---
+++ NuGet/scripts/Global_Resources/myResourceFile.mr-IN.js
@@ -0,0 +1,13 @@
+﻿(function (jqL) {
+    if (jqL == undefined) {
+        console.log("plugin not loaded. Include plugin reference to the web page.")
+        return;
+    }
+
+    // Define all your resource entries as Key value pair. Don't change any other code.
+    jqL.Resources = {
+        ResKey1: "चाचणी 1",
+        ResKey2: "चाचणी 2",
+        ResKey3: "चाचणी 3 <span> Html मजकूर </span>",
+    };
+})(jqueryLocalization); // name for the js
